8ff87e0177cc4ec565844710f65a5f8d2ee435b1
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-10-15 21:12:31 +0300

    create discord bot

--- apps/discord-bot/src/main.ts
+++ apps/discord-bot/src/main.ts
@@ -1,1 +1,22 @@
-console.log('Hello World');
+import { Client, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+import { config } from '@sot-news-bot/shared';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  // const discordPostService = new DiscordPostService(message);
+  // discordPostService.sendPost();
+});
+
+client.login(config.discordBotToken);
