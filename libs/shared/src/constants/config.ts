7535835a908364ff6d5064971f5676831f3e12fd
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-10-15 21:04:03 +0300

    first commit

---
+++ libs/shared/src/constants/config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+export const config = {
+  discordBotToken: process.env['DISCORD_BOT_TOKEN'],
+  telegramBotToken: process.env['TELEGRAM_BOT_TOKEN'],
+  telegramChannelId: process.env['TELEGRAM_CHANNEL_ID'],
+  telegramServerPort: process.env['TELEGRAM_SERVER_PORT'],
+  discordAnnouncementChannelId: process.env['DISCORD_ANNOUNCEMENT_CHANNEL_ID'],
+  discordReleaseNotesChannelId: process.env['DISCORD_RELEASE_NOTES_CHANNEL_ID'],
+  discordGameUpdatesChannelId: process.env['DISCORD_GAME_UPDATES_CHANNEL_ID'],
+  discordLiveEventsChannelId: process.env['DISCORD_LIVE_EVENTS_CHANNEL_ID'],
+};
