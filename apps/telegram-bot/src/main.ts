ff0567e4bcc6d4b91a275d5bf6acd5fc97e50987
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-10-18 20:17:03 +0300

    remove express dependency

--- apps/telegram-bot/src/main.ts
+++ apps/telegram-bot/src/main.ts
@@ -1,18 +1,4 @@
-import express from 'express';
 import 'dotenv/config';
 import { startBot } from './bot';
-import postRoutes from './routes/post.routes';
-import { config } from '@disco-cast-bot/shared';
-
-const app = express();
-app.use(express.json());
-
-app.use(postRoutes);
-
-const PORT = config.telegramServerPort;
-
-app.listen(PORT, () => {
-  console.log(`Telegram bot server is running on port ${PORT}\n`);
-});
 
 startBot();
