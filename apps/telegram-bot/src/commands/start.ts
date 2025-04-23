8137bb06e3863e3ec9304d6fc1cae723968b968f
Vladyslav Harkusha <harkushavlad823@gmail.com>
2025-04-23 18:38:52 +0300

    change start command

--- apps/telegram-bot/src/commands/start.ts
+++ apps/telegram-bot/src/commands/start.ts
@@ -1,10 +1,9 @@
 import { Context } from 'telegraf';
 import { TELEGRAM_INTERACTION_KEYBOARD_MARKUP } from '../constants/telegramConstants';
 import { getOrCreateTelegramUser } from '../services/telegramUser.service';
+import { showKeysCommand } from './showKeys';
 
 export const startCommand = async (ctx: Context) => {
   await getOrCreateTelegramUser(ctx);
-  ctx.reply('Halo', {
-    reply_markup: TELEGRAM_INTERACTION_KEYBOARD_MARKUP,
-  });
+  await showKeysCommand(ctx);
 };
